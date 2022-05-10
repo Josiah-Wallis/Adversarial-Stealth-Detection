0c9f674089ccb32860b17c3cd4ceced9574b53fc
Josiah Wallis <63298097+Josiah-Wallis@users.noreply.github.com>
2022-05-10 10:45:20 -0700

    standard fedavg for CNN

--- asd.py
+++ asd.py
@@ -1,73 +1,103 @@
-# %%Federation and Main Algorithms
+# Federation and Main Algorithms
 import tensorflow as tf
+import numpy as np
+import time
+from random import sample
 from tensorflow import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Activation, Dense, Flatten, BatchNormalization, Conv2D, MaxPool2D
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.metrics import categorical_crossentropy
 from distribute_data import generate_mnist_client_data
+from joblib import Parallel, delayed
 
-# %%
-pkg = generate_mnist_client_data()
+# Generalize later
+# Initializes weights for FedAvg according to CNN parameters
+def initialize_weights():
+    w = []
+    b = []
+    w.append(np.random.randn(3, 3, 1, 8))
+    b.append(np.random.randn(8))
+    w.append(np.random.randn(3, 3, 8, 16))
+    b.append(np.random.randn(16))
+    w.append(np.random.randn(784, 10))
+    b.append(np.random.randn(10))
 
-# %%
-client_train_data = pkg['Client Train Data']
-client_train_labels = pkg['Client Train Labels']
-client_test_data = pkg['Client Test Data']
-client_test_labels = pkg['Client Test Labels']
+    return w, b
 
-# %%
-model = Sequential()
-# was 32 filters
-model.add(Conv2D(filters = 8, kernel_size = (3, 3), activation = 'relu', padding = 'same', input_shape = (28, 28, 1)))
-model.add(MaxPool2D(pool_size = (2, 2), strides = 2))
-# was 64 filters
-model.add(Conv2D(filters = 16, kernel_size = (3, 3), activation = 'relu', padding = 'same'))
-model.add(MaxPool2D(pool_size = (2, 2), strides = 2))
-model.add(Flatten())
-model.add(Dense(units = 10, activation = 'softmax'))
+# Generates CNN with 2 convolutional layers and a dense layer. Initializes weights with passed w, b
+def generate_model(w, b, trainable_layers):
+    model = Sequential([
+        Conv2D(filters = 8, kernel_size = (3, 3), activation = 'relu', padding = 'same', input_shape = (28, 28, 1)),
+        MaxPool2D(pool_size = (2, 2), strides = 2),
+        Conv2D(filters = 16, kernel_size = (3, 3), activation = 'relu', padding = 'same'),
+        MaxPool2D(pool_size = (2, 2), strides = 2),
+        Flatten(),
+        Dense(units = 10, activation = 'softmax')
+    ])
 
-# %%
-model.compile(optimizer = Adam(learning_rate = 0.001), loss = 'categorical_crossentropy', metrics = ['accuracy'])
+    for i, x in enumerate(trainable_layers):
+        model.layers[x].set_weights([w[i], b[i]])
 
-# %%
-model.fit(x = client_train_data[0], y = client_train_labels[0], validation_split = 0.2, epochs = 20, verbose = 2, use_multiprocessing = True)
+    return model
 
-# %%
-model.summary()
+# Trains CNN locally and returns weight updates
+def ClientUpdate(X, y, w, b, B, E, learning_rate, trainable_layers):
+    model = generate_model(w, b, trainable_layers)
+    model.compile(optimizer = Adam(learning_rate = learning_rate), loss = 'categorical_crossentropy', metrics = ['accuracy'])
+    model.fit(X, y, validation_split = 0.2, batch_size = B, epochs = E, verbose = 0, use_multiprocessing = True)
 
-# %%
-l = model.layers[0]
-x = l.weights[0]
-x + x
+    w = []
+    b = []
+    for l in trainable_layers:
+        w.append(model.layers[l].get_weights()[0])
+        b.append(model.layers[l].get_weights()[1])
 
-# %%
-model.layers[0].weights[1]
+    return w, b
 
+# Aggregates weights from all client models
+def aggregate(w_updates, b_updates, n_k, S_t):
+    K = len(S_t)
+    num_trainable_layers = len(w_updates[S_t[0]])
+    w = [0 for _ in range(num_trainable_layers)]
+    b = [0 for _ in range(num_trainable_layers)]
+    n = np.sum(n_k[S_t])
 
-# %%
-for layer in model.layers:
-    if len(layer.weights):
-        print(layer.weights[0].shape)
-        print(layer.weights[1].shape, end = '\n\n')
+    for k in S_t:
+        w_k = w_updates[k]
+        b_k = b_updates[k]
+        for l in range(num_trainable_layers):
+            w[l] += (n_k[k] / n) * w_k[l]
+            b[l] += (n_k[k] / n) * b_k[l]
 
-# %%
-def initialize_w():
-    w = {}
-    w['l1 w'] = tf.random.normal([3, 3, 1, 8], 0, 1, tf.float32)
-    w['l1 b'] = tf.random.normal([8], 0, 1, tf.float32)
-    w['l2 w'] = tf.random.normal([3, 3, 8, 16], 0, 1, tf.float32)
-    w['l2 b'] = tf.random.normal([16], 0 , 1, tf.float32)
-    w['l3 w'] = tf.random.normal([784, 10], 0, 1, tf.float32)
-    w['l3 b'] = tf.random.normal([10], 0, 1, tf.float32)
+    return w, b
 
-    return w
+# Standard Federated Averaging for CNN
+def FedAvg(client_train_data, client_train_labels, batch_size = 100, epochs = 5, learning_rate = 0.001, frac_clients = 1, rounds = 20):
+    w, b = initialize_weights()
+    K = len(client_train_data)
+    m = max(int(frac_clients * K), 1)
+    client_set = range(K)
 
-def FedAvg(data, batch_size, epochs, learning_rate, frac_clients, rounds):
-    w = initialize_w()
-    K = len(data['Client Train Data'])
+    n_k = []
+    for x in client_train_data:
+        n_k.append(x.shape[0])
+    n_k = np.array(n_k)
+
+    trainable_layers = []
+    for i, x in enumerate(generate_model(w, b, trainable_layers).layers):
+        if x.weights:
+            trainable_layers.append(i)
 
     for t in range(rounds):
-        pass
+        print(f'Round {t + 1}')
+        w_updates = [None for _ in range(m)]
+        b_updates = [None for _ in range(m)]
+        S_t = sample(client_set, m)
 
-# %%
+        for k in S_t:
+            w_updates[k], b_updates[k] = ClientUpdate(client_train_data[k], client_train_labels[k], w, b, batch_size, epochs, learning_rate, trainable_layers)
+
+        w, b = aggregate(w_updates, b_updates, n_k, S_t)
+        
+    return w, b
