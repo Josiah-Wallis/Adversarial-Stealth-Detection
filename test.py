9b36224c7a90ffe990a442913d5c36b8cce7c3f3
Josiah Wallis <63298097+Josiah-Wallis@users.noreply.github.com>
2022-06-01 13:53:17 -0700

    turned fedavg and data generation modules into classes

--- test.py
+++ test.py
@@ -1,41 +1,9 @@
 # %% 1-Client Adversarial Stealth Detection (1-CLient ASD) - test run via ipython notebook
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import random
-
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.metrics import categorical_crossentropy
-from tensorflow.keras.utils import to_categorical
-from distribute_data import generate_mnist_client_data, create_adversary
-from fedavg import FedAvg, generate_model
-from tensorflow.keras.datasets import fashion_mnist
+from distribute_data import Datasets
+from fedavg import FederatedSystem
 
 from asd import *
+
 # %%
-x = fashion_mnist.load_data()
-# %%
-x
-# %%
-type(x)
-# %%
-len(x)
-# %%
-thing = x[0]
-# %%
-other = x[1]
-# %%
-thing
-# %%
-other
-# %%
-type(thing[0])
-# %%
-data = pd.read_csv('adult.data', sep = ',', header = None)
-# %%
-data
-# %%
-data[14][50]
+model = FederatedSystem('cifar')
 # %%
